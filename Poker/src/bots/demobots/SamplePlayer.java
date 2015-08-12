2d9c02bc1c952b46b674cc424a214455a1c18840
igalna <igalna@gmail.com>
2015-08-12 19:49:47 +0100

    Working on the pre flop strategy of the SamplePlayer
    Made it quite aggressive.
    
    It will look to bet based on a number of factors.
    - Pocket pairs
    - pocket cards have rank greater than 10
    - pocket cards are within the range of a possible straight

--- Poker/src/bots/demobots/SamplePlayer.java
+++ Poker/src/bots/demobots/SamplePlayer.java
@@ -14,72 +14,97 @@ public class SamplePlayer implements Player {
 	private int seat;
 	private Card c1, 
 				 c2;
-	private GameInfo gi;;
+	private GameInfo gi;
 	private Preferences playerPrefs;
 
+	public static void main(String[] args) {
+		Card a = new Card(1, Card.DIAMONDS);
+		Card b = new Card(4, Card.SPADES);
+		
+		System.out.println(Math.abs(a.getRank() - b.getRank()) < 4);
+	}
 
 	public SamplePlayer() {
 	}
-
+	
 	@Override
-	public void actionEvent(int arg0, Action arg1) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void dealHoleCardsEvent() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void gameOverEvent() {
-		// TODO Auto-generated method stub
-
+	public void holeCards(Card c1, Card c2, int seat) {
+		this.c1 = c1;
+		this.c2 = c2;
+		this.seat = seat;
 	}
 
 	@Override
 	public void gameStartEvent(GameInfo gi) {
 		this.gi = gi;
 	}
+	
+	@Override
+	public Action getAction() {
+		if (gi.isPreFlop()) {
+			return getPreFlopAction();
+		}
+		double toCall = gi.getAmountToCall(seat);
+		return Action.callAction(toCall);
+	}
+
+	private Action getPreFlopAction() {
+		double toCall = gi.getAmountToCall(seat);
+		
+		// if you have pocket pairs, raise.
+		if (c1.getRank() == c2.getRank()) {
+			if (gi.getNumRaises() < 2) {
+				return Action.raiseAction(gi);
+			}
+			if (gi.getNumRaises() < 3) {
+				return Action.callAction(toCall);
+			}
+		}
+		
+		// raise if both hole cards are bigger than 10
+		if (c1.getRank() > Card.TEN && c2.getRank() > Card.TEN) {
+			if (gi.getNumRaises() < 2) {
+				return Action.raiseAction(gi);
+			}
+			return Action.callAction(toCall);
+		}
+		
+		// raise if both hole cards are within range of a straight
+		if (Math.abs(c1.getRank() - c2.getRank()) < 4) {
+			return Action.raiseAction(gi);
+		}
+		
+		
+		return Action.checkOrFoldAction(toCall);
+	}
+
+	@Override
+	public void actionEvent(int arg0, Action arg1) {
+	}
+
+	@Override
+	public void dealHoleCardsEvent() {
+	}
+
+	@Override
+	public void gameOverEvent() {
+	}
+
 
 	@Override
 	public void gameStateChanged() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void showdownEvent(int arg0, Card arg1, Card arg2) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void stageEvent(int arg0) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void winEvent(int arg0, double arg1, String arg2) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Action getAction() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void holeCards(Card c1, Card c2, int seat) {
-		this.c1 = c1;
-		this.c2 = c2;
-		this.seat = seat;
-
 	}
 
 	@Override
