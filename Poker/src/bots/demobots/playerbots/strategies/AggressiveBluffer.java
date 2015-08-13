1fd16472e660eb9532ae99a298914b5d5a0c7df7
igalna <igalna@gmail.com>
2015-08-13 14:27:21 +0100

    Changed the algorithm in Aggressivebluffer
    instead of subclassing Aggressive, it is a new implementation of
    Strategy.
    
    The calls to .super were slow.
    
    copying the implementation to the class instead of calling the
    implmentation of the super class is much faster

--- Poker/src/bots/demobots/playerbots/strategies/AggressiveBluffer.java
+++ Poker/src/bots/demobots/playerbots/strategies/AggressiveBluffer.java
@@ -4,23 +4,55 @@ import com.biotools.meerkat.Action;
 import com.biotools.meerkat.Card;
 import com.biotools.meerkat.GameInfo;
 
-public class AggressiveBluffer extends Aggressive {
+/**
+ * 
+ * Same strategy as the Aggressive, but has an extra bluffing condition.
+ * 
+ * Some percentage of the time it will call regardless of it's hand
+ * 
+ * @author igalna
+ *
+ */
+
+public class AggressiveBluffer implements Strategy {
 
 	@Override
 	public Action getPreFlopAction(Card c1, Card c2, GameInfo gi, int seat) {
 		
-		if ((Math.random()  * 100) < 95) {
-			return  super.getPreFlopAction(c1, c2, gi, seat);
+		double tocall = gi.getAmountToCall(seat);
+		
+		// if you have pocket pairs, raise.
+		if (c1.getRank() == c2.getRank()) {
+			if (gi.getNumRaises() < 2) {
+				return Action.raiseAction(gi);
+			}
+			if (gi.getNumRaises() < 3) {
+				return Action.callAction(tocall);
+			}
 		}
-		return Action.callAction(gi.getAmountToCall(seat));
+		
+		// raise if both hole cards are bigger than 10
+		if (c1.getRank() > Card.TEN && c2.getRank() > Card.TEN) {
+			if (gi.getNumRaises() < 2) {
+				return Action.raiseAction(gi);
+			}
+			return Action.callAction(tocall);
+		}
+		
+		// raise if both hole cards are within range of a straight
+		if (Math.abs(c1.getRank() - c2.getRank()) < 4) {
+			return Action.raiseAction(gi);
+		}
+		
+		if (Math.random() < 0.05) {
+			return Action.callAction(tocall);
+		}
+		
+		return Action.checkOrFoldAction(tocall);
 	}
 	
 	@Override
 	public Action getPostFlopAction(Card c1, Card c2, GameInfo gi, int seat) {
-		
-		if ((Math.random() * 100) < 95) {
-			return super.getPreFlopAction(c1, c2, gi, seat);
-		}
 		return Action.callAction(gi.getAmountToCall(seat));
 	}
 }
