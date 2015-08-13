4847f40c514044474eec3231adcf471355d1797e
igalna <igalna@gmail.com>
2015-08-13 15:57:02 +0100

    Variations of the Aggresive style of play
    -Conservative only plays when pocket pairs are greater than 10
    
    - Liberal plays when pocket pairs are greater than 7

---
+++ Poker/src/bots/demobots/playerbots/strategies/AggressiveConservative.java
@@ -0,0 +1,55 @@
+package bots.demobots.playerbots.strategies;
+
+import com.biotools.meerkat.Action;
+import com.biotools.meerkat.Card;
+import com.biotools.meerkat.GameInfo;
+
+/**
+ * 
+ * Aggressive strategy
+ * 
+ * Implements an aggressive strategy for players
+ * 
+ * 
+ * @author igalna
+ *
+ */
+public class AggressiveConservative implements Strategy {
+
+	@Override
+	public Action getPreFlopAction(Card c1, Card c2, GameInfo gi, int seat) {
+		double toCall = gi.getAmountToCall(seat);
+		
+		// if you have pocket pairs, raise.
+		if (c1.getRank() == c2.getRank()) {
+			if (gi.getNumRaises() < 2) {
+				return Action.raiseAction(gi);
+			}
+			if (gi.getNumRaises() < 3) {
+				return Action.callAction(toCall);
+			}
+		}
+		
+		// raise if both hole cards are bigger than Ten
+		if (c1.getRank() > Card.TEN && c2.getRank() > Card.TEN) {
+			if (gi.getNumRaises() < 2) {
+				return Action.raiseAction(gi);
+			}
+			return Action.callAction(toCall);
+		}
+		
+		// raise if both hole cards are within range of a straight
+		if (Math.abs(c1.getRank() - c2.getRank()) < 4) {
+			return Action.raiseAction(gi);
+		}
+		
+		
+		return Action.checkOrFoldAction(toCall);
+	}
+
+	@Override
+	public Action getPostFlopAction(Card c1, Card c2, GameInfo gi, int seat) {
+		return Action.callAction(gi.getAmountToCall(seat));
+	}
+
+}
