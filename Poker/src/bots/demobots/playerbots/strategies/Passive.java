16f0a6bad5ddc957ec34e8fa4eb1eec32659477a
igalna <igalna@gmail.com>
2015-08-13 14:35:11 +0100

    Passive strategy.
    Calls if it has 
    -pocket pairs
    -both hole cards greater than 10
    -both hold cards in range of a straight
    
    checks otherwise

---
+++ Poker/src/bots/demobots/playerbots/strategies/Passive.java
@@ -0,0 +1,39 @@
+package bots.demobots.playerbots.strategies;
+
+import com.biotools.meerkat.Action;
+import com.biotools.meerkat.Card;
+import com.biotools.meerkat.GameInfo;
+
+public class Passive implements Strategy {
+
+	@Override
+	public Action getPreFlopAction(Card c1, Card c2, GameInfo gi, int seat) {
+		
+		double toCall = gi.getAmountToCall(seat);
+		
+		// if you have pocket pairs, call
+		if (c1.getRank() == c2.getRank()) {
+			return Action.callAction(toCall);
+		}
+		
+		// if both hole cards are bigger than 10, call
+		if (c1.getRank() > Card.TEN && c2.getRank() > Card.TEN) {
+			return Action.callAction(toCall);
+		}
+		
+		// raise if both hole cards are within range of a straight
+		if (Math.abs(c1.getRank() - c2.getRank()) < 4) {
+			return Action.callAction(toCall);
+		}
+		
+		
+		return Action.checkOrFoldAction(toCall);
+		
+	}
+
+	@Override
+	public Action getPostFlopAction(Card c1, Card c2, GameInfo gi, int seat) {
+		return Action.callAction(gi.getAmountToCall(seat));
+	}
+
+}
